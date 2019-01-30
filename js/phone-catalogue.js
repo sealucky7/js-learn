ac9eab83461da5ac612d76476ed6ac068c33271e
sea-lucky <sea.yastreb@gmail.com>
2019-01-30 22:15:01 +0300

    load lodash template

--- js/phone-catalogue.js
+++ js/phone-catalogue.js
@@ -3,14 +3,18 @@
 class PhoneCatalogue {
   constructor(options) {
     this._el = options.el;
+    this._phones = options.phones;
     this._render();
   }
 
   _render() {
 
     const template = document.querySelector('#phone-catalogue-template').innerHTML;
+    const templateFunction = _.template(template);
 
-    this._el.innerHTML = template;
+    this._el.innerHTML = templateFunction({
+      phones: this._phones,
+    });
   }
 
 }
