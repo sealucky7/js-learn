bffd5c95a458f4430d7661a4fb3c5bab9100320f
sea-lucky <sea.yastreb@gmail.com>
2019-01-30 22:22:57 +0300

    small changes

--- js/phone-catalogue.js
+++ js/phone-catalogue.js
@@ -4,15 +4,14 @@ class PhoneCatalogue {
   constructor(options) {
     this._el = options.el;
     this._phones = options.phones;
+    const template = document.querySelector('#phone-catalogue-template').innerHTML;
+    this._compiledFunction = _.template(template);
     this._render();
   }
 
   _render() {
 
-    const template = document.querySelector('#phone-catalogue-template').innerHTML;
-    const templateFunction = _.template(template);
-
-    this._el.innerHTML = templateFunction({
+    this._el.innerHTML = this._compiledFunction({
       phones: this._phones,
     });
   }
